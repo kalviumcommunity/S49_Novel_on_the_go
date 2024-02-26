68e71058a0ed076fca47747c3656117844545992
Adithi M <adithimuralidhara@gmail.com>
2024-02-26 12:59:11 +0530

    used components

--- Novels/src/App.jsx
+++ Novels/src/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import backgroundImage from './books.jpg'; 
-import './App.css'; 
+import WelcomeComponent from './welcome';
+import EntityComponent from './data';
 
-const WelcomeComponent = () => {
-  const containerStyle = {
-    backgroundImage: `url(${backgroundImage})`
-  };
-
+function App() {
   return (
-    <div className="main">
-    <div className='container' style={containerStyle}>
-      <h1>Welcome to Novels To Go</h1>
-      <p>Discover a world of stories at your fingertips</p>
-    </div>
+    <div>
+      <WelcomeComponent />
+      <EntityComponent />
     </div>
   );
-};
+}
 
-export default WelcomeComponent;
+export default App;
